1414c9539e0a1664fb747399d6626a6ff078ffbd
achozen <achozen93@gmail.com>
2015-07-09 17:03:29 +0200

    Added full functionality with tests

--- src/bowling/game/main/Game.java
+++ src/bowling/game/main/Game.java
@@ -3,14 +3,51 @@ package bowling.game.main;
 public class Game {
 
     private int score = 0;
-    
-    public void roll(int pins){
-	
-	score +=pins;
-	
+
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+	rolls[currentRoll++] = pins;
+
     }
-    public int score (){
-	
+
+    public int score() {
+	int rollNumber = 0;
+	for (int frame = 0; frame < 10; frame++) {
+	    if (isStrike(rollNumber)) {
+		score += 10 + bonusForStrike(rollNumber);
+		rollNumber++;
+	    } else if (isSpare(rollNumber)) {
+		score += 10 + bonusForSpare(rollNumber);
+		rollNumber += 2;
+	    } else {
+		score += commonSumming(rollNumber);
+		rollNumber += 2;
+	    }
+	}
 	return score;
     }
+
+    private boolean isStrike(int rollNumber) {
+	return rolls[rollNumber] == 10;
+    }
+
+    private boolean isSpare(int rollNumber) {
+	return rolls[rollNumber] + rolls[rollNumber + 1] == 10;
+    }
+
+    private int bonusForStrike(int rollNumber) {
+	return rolls[rollNumber + 1] + rolls[rollNumber + 2];
+    }
+
+    private int bonusForSpare(int rollNumber) {
+	return rolls[rollNumber + 2];
+    }
+
+    private int commonSumming(int rollNumber) {
+	return rolls[rollNumber] + rolls[rollNumber + 1];
+
+    }
+
 }
--- src/bowling/game/tests/BowlingGameTest.java
+++ src/bowling/game/tests/BowlingGameTest.java
@@ -3,23 +3,75 @@ package bowling.game.tests;
 import bowling.game.main.Game;
 import junit.framework.TestCase;
 
-public class BowlingGameTest extends TestCase{
-    
+public class BowlingGameTest extends TestCase {
+
     private Game g;
-    
-    protected void setUp() throws Exception{
-	
-	g= new Game();
-	
+
+    protected void setUp() throws Exception {
+	g = new Game();
     }
 
-    public void testGutterGame() throws Exception{
-	for (int i=0;i<20;i++)g.roll(0);	
+    public void rollMany(int n, int pins) {
+	for (int i = 0; i < n; i++)
+	    g.roll(pins);
+    }
+
+    public void testGutterGame() throws Exception {
+	rollMany(20, 0);
 	assertEquals(0, g.score());
     }
-    
-    public void testAllones() throws Exception{
-	for (int i=0;i<20;i++)g.roll(1);
+
+    public void testAllones() throws Exception {
+	rollMany(20, 1);
 	assertEquals(20, g.score());
+    }
+
+    public void testOnSpare() throws Exception {
+	rollSpare();
+	g.roll(3);
+	rollMany(17, 0);
+	assertEquals(16, g.score());
+    }
+
+    public void testRollStrike() throws Exception {
+	g.roll(5);
+	g.roll(4);
+	rollStrike();
+	g.roll(3);
+	rollMany(15, 0);
+	assertEquals(25, g.score());
+    }
+
+    public void testOneStrike() throws Exception {
+	g.roll(10);
+	g.roll(3);
+	g.roll(4);
+	rollMany(16, 0);
+	assertEquals(24, g.score());
+    }
+    
+    public void testForPerfectGame(){
+	rollMany(12,10);
+	assertEquals(300,g.score());
+	
+    }
+    
+    public void testWithBonuses(){
+	
+	g.roll(5);
+	g.roll(10);
+    	g.roll(3);
+    	g.roll(7);
+    	rollMany(15,1);
+    	assertEquals(46,g.score());
+    }
+
+    public void rollSpare() {
+	g.roll(5);
+	g.roll(5);
+    }
+
+    public void rollStrike() {
+	g.roll(10);
     }
 }
