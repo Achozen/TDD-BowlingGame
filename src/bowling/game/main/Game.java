bd5a445e705f75fbd56e7a0713253f6d95c74b80
achozen <achozen93@gmail.com>
2015-07-09 12:43:55 +0200

    Game class and test class created.

---
+++ src/bowling/game/main/Game.java
@@ -0,0 +1,16 @@
+package bowling.game.main;
+
+public class Game {
+
+    private int score = 0;
+    
+    public void roll(int pins){
+	
+	score +=pins;
+	
+    }
+    public int score (){
+	
+	return score;
+    }
+}
